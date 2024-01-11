e062901ac9d28caa643b264738d93f155b040cc0
Emmar Caber <caberemmar@gmail.com>
2024-01-11 15:42:53 +0800

    feat: add validation and retain old value even after closing window

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [page, setPage] = useState(1);
@@ -13,12 +13,45 @@ function App() {
   const [collegeAddress, setCollegeAddress] = useState("");
   const [collegeProgram, setCollegeProgram] = useState("");
 
+  useEffect(() => {
+    const oldPage = parseInt(localStorage.getItem("page")) || 1;
+
+    // Personal Information Old Values
+    const oldFirstName = localStorage.getItem("firstName") || "";
+    const oldMiddleName = localStorage.getItem("middleName") || "";
+    const oldLastName = localStorage.getItem("lastName") || "";
+    const oldFatherName = localStorage.getItem("fatherName") || "";
+    const oldMotherName = localStorage.getItem("motherName") || "";
+
+    const oldContactNumber = localStorage.getItem("contactNumber") || "";
+    const oldEmailAddress = localStorage.getItem("emailAddress") || "";
+
+    const oldCollegeName = localStorage.getItem("collegeName") || "";
+    const oldCollegeAddress = localStorage.getItem("collegeAddress") || "";
+    const oldCollegeProgram = localStorage.getItem("collegeProgram") || "";
+
+    setFirstName(() => oldFirstName);
+    setMiddleName(() => oldMiddleName);
+    setLastName(() => oldLastName);
+    setFatherName(() => oldFatherName);
+    setMotherName(() => oldMotherName);
+    setContactNumber(() => oldContactNumber);
+    setEmailAddress(() => oldEmailAddress);
+    setCollegeName(() => oldCollegeName);
+    setCollegeAddress(() => oldCollegeAddress);
+    setCollegeProgram(() => oldCollegeProgram);
+  });
+
   function nextStep() {
-    if (page >= 1) setPage((p) => p + 1);
+    if (page >= 1) {
+      setPage((p) => p + 1);
+    }
   }
 
   function prevStep() {
-    if (page <= 3) setPage((p) => p - 1);
+    if (page <= 3) {
+      setPage((p) => p - 1);
+    }
   }
 
   function alertMessage(text) {
@@ -26,10 +59,9 @@ function App() {
   }
 
   function page1NextStep() {
+    const validRegex = /[a-z][A-z]+/;
     if (firstName === "") {
       alertMessage("First Name is required!");
-    } else if (middleName === "") {
-      alertMessage("Middle Name is required!");
     } else if (lastName === "") {
       alertMessage("Last Name is required!");
     } else if (fatherName === "") {
@@ -42,34 +74,20 @@ function App() {
   }
 
   function page2NextStep() {
+    const validNumber = /[0-9]+/;
+    const validEmail =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
     if (contactNumber === "") {
       alertMessage("Contact Number is required!");
+    } else if (!validNumber.test(contactNumber)) {
+      alertMessage("Contact Number must have digits only!");
+    } else if (!validEmail.test(emailAddress)) {
+      alertMessage("Email Address is invalid!");
     } else if (emailAddress === "") {
       alertMessage("Email Address is required!");
     } else {
       nextStep();
-    }
-  }
-
-  function page2PrevStep() {
-    if (contactNumber === "") {
-      alertMessage("Contact Number is required!");
-    } else if (emailAddress === "") {
-      alertMessage("Email Address is required!");
-    } else {
-      prevStep();
-    }
-  }
-
-  function page3PrevStep() {
-    if (collegeName === "") {
-      alertMessage("College Name is required!");
-    } else if (collegeAddress === "") {
-      alertMessage("Address is required!");
-    } else if (collegeProgram === "") {
-      alertMessage("Program is required!");
-    } else {
-      prevStep();
     }
   }
 
@@ -87,9 +105,10 @@ function App() {
   }
 
   function createNew() {
+    localStorage.clear();
     setPage((p) => 1);
 
-    // Back to empty values
+    // Empty state values
     setFirstName("");
     setMiddleName("");
     setLastName("");
@@ -103,52 +122,82 @@ function App() {
   }
 
   function handleFirstName(e) {
-    setFirstName(() => e.target.value);
+    const first_name = e.target.value;
+    setFirstName(() => first_name);
+    localStorage.setItem("firstName", first_name);
   }
 
   function handleMiddleName(e) {
-    setMiddleName(() => e.target.value);
+    const middle_name = e.target.value;
+    setMiddleName(() => middle_name);
+    localStorage.setItem("middleName", middle_name);
   }
 
   function handleLastName(e) {
-    setLastName(() => e.target.value);
+    const last_name = e.target.value;
+    setLastName(() => last_name);
+    localStorage.setItem("lastName", last_name);
   }
 
   function handleFatherName(e) {
-    setFatherName(() => e.target.value);
+    const father_name = e.target.value;
+    setFatherName(() => father_name);
+    localStorage.setItem("fatherName", father_name);
   }
 
   function handleMotherName(e) {
-    setMotherName(() => e.target.value);
+    const mother_name = e.target.value;
+    setMotherName(() => mother_name);
+    localStorage.setItem("motherName", mother_name);
   }
 
   function handleContactNumber(e) {
-    setContactNumber(() => e.target.value);
+    const contact_number = e.target.value;
+    setContactNumber(() => contact_number);
+    localStorage.setItem("contactNumber", contact_number);
   }
 
   function handleEmailAddress(e) {
-    setEmailAddress(() => e.target.value);
+    const email_address = e.target.value;
+    setEmailAddress(() => email_address);
+    localStorage.setItem("emailAddress", email_address);
   }
 
   function handleCollegeName(e) {
-    setCollegeName(() => e.target.value);
+    const college_name = e.target.value;
+    setCollegeName(() => college_name);
+    localStorage.setItem("collegeName", college_name);
   }
 
   function handleCollegeAddress(e) {
-    setCollegeAddress(() => e.target.value);
+    const college_address = e.target.value;
+    setCollegeAddress(() => college_address);
+    localStorage.setItem("collegeAddress", college_address);
   }
 
   function handleCollegeProgram(e) {
-    setCollegeProgram(() => e.target.value);
+    const college_program = e.target.value;
+    setCollegeProgram(() => college_program);
+    localStorage.setItem("collegeProgram", college_program);
   }
 
   switch (page) {
     case 1:
       return (
         <div className="container mt-5">
+          <h1 className="text-center py-3">Multi-Step Form</h1>
           <div className="card p-3 my-3">
-            <h3 className="card-title mb-3">Personal Information</h3>
-            <div className="row align-items-start">
+            <h3 className="card-title mb-3">
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item">Step {page}</li>
+                  <li class="breadcrumb-item active" aria-current="page">
+                    Personal Information
+                  </li>
+                </ol>
+              </nav>
+            </h3>
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 align-items-start">
               <div className="mb-3 col">
                 <label for="firstName" className="form-label">
                   First Name
@@ -157,6 +206,7 @@ function App() {
                   type="text"
                   className="form-control"
                   id="firstName"
+                  placeholder="Ex. Emmar"
                   value={firstName}
                   onChange={(e) => handleFirstName(e)}
                 />
@@ -164,12 +214,13 @@ function App() {
 
               <div className="mb-3 col">
                 <label for="middleName" className="form-label">
-                  Middle Name
+                  Middle Name (optional)
                 </label>
                 <input
                   type="text"
                   className="form-control"
                   id="middleName"
+                  placeholder="Ex. Baracina"
                   value={middleName}
                   onChange={(e) => handleMiddleName(e)}
                 />
@@ -183,6 +234,7 @@ function App() {
                   type="text"
                   className="form-control"
                   id="lastName"
+                  placeholder="Ex. Caber"
                   value={lastName}
                   onChange={(e) => handleLastName(e)}
                 />
@@ -191,12 +243,13 @@ function App() {
 
             <div className="mb-3">
               <label for="fatherName" className="form-label">
-                Father Name
+                Father's Name
               </label>
               <input
                 type="text"
                 className="form-control"
                 id="fatherName"
+                placeholder="Ex. Emmar Baracina Caber"
                 value={fatherName}
                 onChange={(e) => handleFatherName(e)}
               />
@@ -204,19 +257,23 @@ function App() {
 
             <div className="mb-3">
               <label for="motherName" className="form-label">
-                Mother Name
+                Mother's Maiden Name
               </label>
               <input
                 type="text"
                 className="form-control"
                 id="motherName"
+                placeholder="Ex. Emmar Caber Baracina"
                 value={motherName}
                 onChange={(e) => handleMotherName(e)}
               />
             </div>
 
-            <button className="btn btn-primary mt-3" onClick={page1NextStep}>
-              Next Step
+            <button
+              className="btn btn-primary mt-3 py-2"
+              onClick={page1NextStep}
+            >
+              Next
             </button>
           </div>
         </div>
@@ -225,8 +282,18 @@ function App() {
     case 2:
       return (
         <div className="container mt-5">
+          <h1 className="text-center py-3">Multi-Step Form</h1>
           <div className="card p-3 my-3">
-            <h3 className="card-title mb-3">Contact Information</h3>
+            <h3 className="card-title mb-3">
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item">Step {page}</li>
+                  <li class="breadcrumb-item active" aria-current="page">
+                    Contact Information
+                  </li>
+                </ol>
+              </nav>
+            </h3>
             <div className="row align-items-start">
               <div className="mb-3">
                 <label for="contactNumber" className="form-label">
@@ -236,6 +303,7 @@ function App() {
                   type="text"
                   className="form-control"
                   id="contactNumber"
+                  placeholder="Ex. 09297893496"
                   value={contactNumber}
                   onChange={(e) => handleContactNumber(e)}
                   required
@@ -250,6 +318,7 @@ function App() {
                   type="text"
                   className="form-control"
                   id="emailAddress"
+                  placeholder="Ex. name@example.com"
                   value={emailAddress}
                   onChange={(e) => handleEmailAddress(e)}
                   required
@@ -257,7 +326,7 @@ function App() {
               </div>
 
               <div className="mt-3 d-flex justify-content-between">
-                <button className="btn btn-secondary" onClick={page2PrevStep}>
+                <button className="btn btn-secondary" onClick={prevStep}>
                   Previous
                 </button>
                 <button className="btn btn-primary" onClick={page2NextStep}>
@@ -272,8 +341,18 @@ function App() {
     case 3:
       return (
         <div className="container mt-5">
+          <h1 className="text-center py-3">Multi-Step Form</h1>
           <div className="card p-3 my-3">
-            <h3 className="card-title mb-3">Educational Background</h3>
+            <h3 className="card-title mb-3">
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item">Step {page}</li>
+                  <li class="breadcrumb-item active" aria-current="page">
+                    Educational Background
+                  </li>
+                </ol>
+              </nav>
+            </h3>
             <div className="row align-items-start">
               <div className="mb-3">
                 <label for="collegeName" className="form-label">
@@ -283,6 +362,7 @@ function App() {
                   type="text"
                   className="form-control"
                   id="collegeName"
+                  placeholder="Ex. Camarines Sur Polytechnic Colleges"
                   value={collegeName}
                   onChange={(e) => handleCollegeName(e)}
                   required
@@ -291,12 +371,13 @@ function App() {
 
               <div className="mb-3">
                 <label for="collegeAddress" className="form-label">
-                  Address
+                  College Address
                 </label>
                 <input
                   type="text"
                   className="form-control"
                   id="collegeAddress"
+                  placeholder="Ex. San Miguel, Nabua, Camarines Sur"
                   value={collegeAddress}
                   onChange={(e) => handleCollegeAddress(e)}
                   required
@@ -311,6 +392,7 @@ function App() {
                   type="text"
                   className="form-control"
                   id="collegeProgram"
+                  placeholder="Ex. Bachelor of Science in Information Technology"
                   value={collegeProgram}
                   onChange={(e) => handleCollegeProgram(e)}
                   required
@@ -318,7 +400,7 @@ function App() {
               </div>
 
               <div className="mt-3 d-flex justify-content-between">
-                <button className="btn btn-secondary" onClick={page3PrevStep}>
+                <button className="btn btn-secondary" onClick={prevStep}>
                   Previous
                 </button>
                 <button className="btn btn-primary" onClick={submit}>
@@ -334,9 +416,10 @@ function App() {
     default:
       return (
         <div className="container mt-5">
+          <h1 className="text-center py-3">Result</h1>
           <div class="card text-bg-success mb-3">
             <div class="card-header fw-bold text-uppercase">
-              Personal Information
+              <h4 className="m-0">Personal Information</h4>
             </div>
             <div class="card-body bg-light text-dark">
               <p>
@@ -348,7 +431,7 @@ function App() {
           </div>
           <div class="card text-bg-warning mb-3">
             <div class="card-header fw-bold text-uppercase">
-              Contact Information
+              <h4 className="m-0">Contact Information</h4>
             </div>
             <div class="card-body bg-light text-dark">
               <p>Email Address: {emailAddress}</p>
@@ -358,7 +441,7 @@ function App() {
 
           <div class="card text-bg-info mb-3">
             <div class="card-header text-light fw-bold text-uppercase">
-              Educational Background
+              <h4 className="m-0">Educational Background</h4>
             </div>
             <div class="card-body bg-light text-dark">
               <p>College Name: {collegeName}</p>
@@ -368,8 +451,8 @@ function App() {
           </div>
 
           <div class="row mx-auto">
-            <button class="btn btn-primary py-2" onClick={createNew}>
-              <h5> + New</h5>
+            <button class="btn btn-primary py-3" onClick={createNew}>
+              <h5 className="m-0"> + New</h5>
             </button>
           </div>
         </div>
